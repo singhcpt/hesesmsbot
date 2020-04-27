8be105e67ae15d5718ed86a5da79cc6ee6e7d103
Benjamin Medoff <medofbro@gmail.com>
2020-04-26 21:21:32 -0400

    fix

--- chirpdataservices.py
+++ chirpdataservices.py
@@ -94,7 +94,7 @@ def get_verifications(event_id):
     verificationCnx = create_connection()
     cursor = verificationCnx.cursor()
 
-    get_verifications = "SELECT (event_id, user_id, verified) FROM Verifications WHERE event_id = " + str(event_id) + ";"
+    get_verifications = "SELECT event_id, user_id, verified FROM Verifications WHERE event_id = " + str(event_id) + ";"
 
     print(get_verifications)
 
@@ -113,7 +113,7 @@ def get_users_verified(event_id):
     verificationCnx = create_connection()
     cursor = verificationCnx.cursor()
 
-    get_verifications = "SELECT (user_id) FROM Verifications WHERE event_id = " + str(event_id) + ";"
+    get_verifications = "SELECT user_id FROM Verifications WHERE event_id = " + str(event_id) + ";"
 
     cursor.execute(get_verifications)
 
